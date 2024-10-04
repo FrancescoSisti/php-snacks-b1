45e3082ee41c438b8a73152d9855637de017bcb3
FrancescoSisti <francescosisti61@gmail.com>
2024-10-04 16:31:36 +0200

    first commit

---
+++ snack1.php
@@ -0,0 +1,52 @@
+<?php
+$posts = [
+
+    '10/01/2019' => [
+        [
+            'title' => 'Post 1',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 1'
+        ],
+        [
+            'title' => 'Post 2',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 2'
+        ],
+    ],
+    '10/02/2019' => [
+        [
+            'title' => 'Post 3',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 3'
+        ]
+    ],
+    '15/05/2019' => [
+        [
+            'title' => 'Post 4',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 4'
+        ],
+        [
+            'title' => 'Post 5',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 5'
+        ],
+        [
+            'title' => 'Post 6',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 6'
+        ]
+    ],
+];
+
+foreach ($posts as $date => $dailyPosts) {
+    $formattedDate = date('d-m-Y', strtotime($date));
+    echo "<h2>$formattedDate</h2>";
+
+    foreach ($dailyPosts as $post) {
+        echo "<h3>{$post['title']}</h3>";
+        echo "<p>Autore: {$post['author']}</p>";
+        echo "<p>{$post['text']}</p>";
+        echo "<hr>";
+    }
+}
